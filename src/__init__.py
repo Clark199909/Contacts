586599a04e85c82603364990bd9362504ef3f90e
吴迪 <wudi@wudideMBP.lan>
2022-11-15 02:52:39 -0500

    comments added and little bugs fixed

--- src/__init__.py
+++ src/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__,
             static_url_path='/',
             static_folder='static/class-ui/',
             template_folder='web/templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:20176098wd@localhost/contacts'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/contacts'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
