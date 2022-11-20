107da0f49f21d7c54430ffbf498e685751311c47
Zhengkai Xie <xiezhengkai199982@gmail.com>
2022-11-19 23:15:00 -0500

    delete add student api

--- src/resources/student_resource.py
+++ src/resources/student_resource.py
@@ -7,11 +7,5 @@ class StudentResource:
         pass
 
     @staticmethod
-    def add_new_student(uni):
-        student = Student(uni=uni)
-        db.session.add(student)
-        db.session.commit()
-
-    @staticmethod
     def search_student_by_uni(uni):
         return db.session.query(Student).filter_by(uni=uni).first()
